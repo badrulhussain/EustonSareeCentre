add1219afa1e93a2f2bc044293c526260c813e12
BadrulHussain <Badrul.Hussain1@gmail.com>
2020-06-02 03:50:13 +0100

    Created project

---
+++ EustonSareeCentre/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace EustonSareeCentre.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
